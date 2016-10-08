82e5c863d1f975667ddbb1706344b659a3258998
Zac Collins <Zac Collins>
2016-10-08 10:08:38 -0400

    Updates to personal project using alias

--- maps/public/js/vue/vue-map.js
+++ maps/public/js/vue/vue-map.js
@@ -1,15 +1,11 @@
 var map = new Vue({
     el: 'body',
-    components: {
-        'map': {
-            template: '<div id="map"></div>'
-        },
-    },   
     ready: function() {
         console.log('map component loaded');
         this.initMap();
     },  
     methods: {
+        //Initialize google map
         initMap: function() {
             var map = new google.maps.Map(document.getElementById('map'), {
                 center: { lat: -34.397, lng: 150.644 },
@@ -29,19 +25,23 @@ var map = new Vue({
                     infoWindow.setContent('Location found.');
                     map.setCenter(pos);
                 }, function() {
-                    handleLocationError(true, infoWindow, map.getCenter());
+                    this.handleLocationError(true, infoWindow, map.getCenter());
                 });
             } else {
                 // Browser doesn't support Geolocation
-                handleLocationError(false, infoWindow, map.getCenter());
+                this.handleLocationError(false, infoWindow, map.getCenter());
             }
         },
+        //Initialize google map
         handleLocationError: function(browserHasGeolocation, infoWindow, pos) {
             infoWindow.setPosition(pos);
             infoWindow.setContent(browserHasGeolocation ?
                 'Error: The Geolocation service failed.' :
                 'Error: Your browser doesn\'t support geolocation.');
-        }
+        },
+        /**
+         * 
+        */
     },
 
 });
