87e26505aa5389d0b96bc11cd3024ece83812192
Zac Collins <Zac Collins>
2016-10-08 23:10:07 -0400

    Updates to personal project using alias

--- app/resources/views/app/dashboard.blade.php
+++ app/resources/views/app/dashboard.blade.php
@@ -163,9 +163,9 @@
 
   <footer-component>
       <!-- Libraries -->
-      <script src="/js/three.min.js"></script>
     	<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
       <script src="/js/jquery.min.js"></script>
+      <script src="/js/three.min.js"></script>
       <script src="/js/tablesorter.min.js"></script>
       <script src="/js/toolkit.js"></script>
       <script src="/js/application.js"></script>
